ed5c3396e62b29d51ce50c1164d046920de71c04
Hongbusi <q951686662@qq.com>
2021-03-16 16:28:49 +0800

    moment

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import moment from 'moment';
+moment.locale('zh-cn');
+Vue.prototype.$moment = moment;
+
 import './styles/index.less'; // global css
 import i18n from './lang'; // internationalization
 import './icons'; // iconfont
